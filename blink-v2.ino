6b8b230f72215c53fbc4452db3a692a7b5881422
Dennis Medina <dcortezjr@tutamail.com>
2026-03-06 18:18:52 -0800

    add blink-v2 - delayed blink utilizing variables

---
+++ blink-v2.ino
@@ -0,0 +1,16 @@
+int ledPin = 13;
+int delayPeriod = 100;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop ()
+{
+  digitalWrite(ledPin, HIGH);
+  delay(delayPeriod);
+  digitalWrite(ledPin, LOW);
+  delay(delayPeriod);
+  delayPeriod = delayPeriod + 100;
+}
